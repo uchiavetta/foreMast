974e53cf31fb122abbf623afbaa6bb4c31576a64
Sebastianmarzini <sebastianmarzini@gmail.com>
2021-06-25 08:22:51 +0200

    prova descrizione pacchetto nell'helper

---
+++ R/package_foreMast.R
@@ -0,0 +1,11 @@
+#' foreMast: A package for predicting the probability of seed production of European beech forests
+#' The foreMast package provides three functions:
+#' cdsDownload, mastFaSyl, mastPlot.
+#'
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name foreMast
+NULL
+#> NULL

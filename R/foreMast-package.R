2e3e50e1c80fae6f0805fef2754b8fb771ff4918
Sebastianmarzini <sebastianmarzini@gmail.com>
2021-06-25 09:35:48 +0200

    doc package

--- R/foreMast-package.R
+++ R/foreMast-package.R
@@ -1,3 +1,11 @@
+#'@title foreMast
+#'This package allow to calculate the European beech (Fagus sylvatica L.) seed production probability in the current
+#'year for a given site
+
+
+
+
+
 #' @keywords internal
 "_PACKAGE"
 

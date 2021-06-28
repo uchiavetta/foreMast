f7cd25de2bf46a0966e981b3c7c69c6aa26a6726
Sebastianmarzini <sebastianmarzini@gmail.com>
2021-06-28 12:14:16 +0200

    update tests

--- tests/testthat/test-mastPlot.R
+++ tests/testthat/test-mastPlot.R
@@ -1,4 +1,4 @@
 test_that("plots the char of the predicted mast probability", {
-  probPlot <- mastPlot(mastFaSyl("inst/85930_t2s_tp.nc"))
+  probPlot <- mastPlot(mastFaSyl("85930_t2s_tp.nc"))
   plot(probPlot)
 })
